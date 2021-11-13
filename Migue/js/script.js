25a5d669aa0ef57164b46786b16df94c363329d8
MiguelALozano <miguelartedigital@gmail.com>
2021-11-13 12:50:07 +0100

    actividad Migue terminada

--- Migue/js/script.js
+++ Migue/js/script.js
@@ -1,37 +1,60 @@
 function validarCampo(){
         let ok = true;
-
+        //let validaNombre = /^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/gi;
+        
+        //Validamos teléfono que este relleno y formato correcto
+        const validaNombre = /^[A-Z]/;
         if(nombre.value.trim()== ""){
             document.getElementById("nombre").placeholder="Campo obligatorio";           
             document.getElementById("nombre").classList.add("error");
             ok = false;
+        }else if(!validaNombre.test(document.getElementById("nombre").value)){
+            console.log(document.getElementById("nombre").value);
+            console.log("El nombre no empieza por mayuscula");
+            document.getElementById("nombre").classList.add("error");
+            document.getElementById("nombre").value = ''
+            document.getElementById("nombre").placeholder="Debe empezar por mayúscula";
+            ok = false;  
         }
-        
-        if(direccion.value.trim()== "") {
+
+        //Validamos dirección que este rellena
+        if(direccion.value.trim()== "" || direccion.value.trim()== " ") {
             document.getElementById("direccion").placeholder="Campo obligatorio";
             document.getElementById("direccion").classList.add("error");
-
             ok = false;
         }
+
+        //Validamos teléfono que este relleno y formato correcto
+        const validaTel = /^[6-9][0-9]{8}$/;
         if(tel.value.trim()== "") {
             document.getElementById("tel").placeholder="Campo obligatorio";
             document.getElementById("tel").classList.add("error");
             ok = false;
+        }else if(!validaTel.test(document.getElementById("tel").value)){
+            console.log(document.getElementById("tel").value);
+            console.log("El tel no es formato correcto");
+            document.getElementById("tel").classList.add("error");
+            document.getElementById("tel").value = ''
+            document.getElementById("tel").placeholder="Teléfono no correcto";
+            ok = false;  
         }
+        
+        //Validamos email que este relleno y formato correcto
+        const validaMail= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if(email.value.trim()== "") {
             document.getElementById("email").placeholder="Campo obligatorio";
             document.getElementById("email").classList.add("error");
             ok = false;
+        }else if(!validaMail.test(document.getElementById("email").value)){
+            console.log(document.getElementById("email").value);
+            console.log("El email no tiene el formato correcto");
+            document.getElementById("email").classList.add("error");
+            document.getElementById("email").value = ''
+            document.getElementById("email").placeholder="E-mail no correcto";
+            ok = false;  
         }
         return ok;
-
-    }
-
-    // input pierda focus o keyup
-    const validaNombre = /^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/gi;
-    if(!validaNombre.test(document.getElementById("nombre").value)){
-        //alert("El nombre tiene que empezar con Mayúscula")
-    }
+}
 
 
 // Validamos que haya un radio seleccionado
@@ -45,7 +68,6 @@ function validarRadio(){
         if(radio[i].checked){
             seleccionado = true;
             document.getElementById("tamanoPizza").classList.remove("error");
-            //break;
         }
     }    
     if(!seleccionado){
@@ -54,12 +76,6 @@ function validarRadio(){
             document.getElementById("tamanoPizza").classList.add("error");
             return false
         }    
-    
-    /*else{
-            document.getElementById("tamanoPizza").classList.add("error");
-            alert('Por favor. Selecciona un tamaño de pizza')
-            return false
-        }*/
 }
 
 // Validamos que haya un checked seleccionado
@@ -73,7 +89,6 @@ function validarChecked(){
         if(ingredienteCheck[i].checked){
             valor++
             document.getElementById("ingredientes").classList.remove("error");
-            //break;
         }
     }
     if(valor < 1){
@@ -81,46 +96,36 @@ function validarChecked(){
         document.getElementById("ingredientes").classList.add("error");
         console.log("Valor: " + valor +  ` y no seleccionado: ${NoSeleccionado}`);
     }
-        /*if(!NoSeleccionado){
-            document.getElementById("ingredientes").classList.add("error");
-            return false
-        }else{
-            // document.getElementById("ingredientes").style="background-color: var(--color-principal);";
-            //document.getElementById("ingredientes").classList.remove("error");
-            console.log("Estoy aquí " + valor);
-            break;
-        }*/
-
 }
 
-
-
-
-
 // obtenemos los importes
+let seleccion
+let importeTotal
 function importe(){
     let importeMasa = 0
     let importeIngredientes = 0
-    let importeTotal
     let element = document.getElementsByName("size")
     console.log(element)
 
     for(var i=0; i<element.length; i++) {       
         if(element[i].checked){
             if(element[i].value == "small"){
+                seleccion = "pequeña"
                 importeMasa += 5;
                 // console.log(`IMPORTE1 ${importeMasa}`)
             } else if(element[i].value == "medium") {
+                seleccion = "mediana"
                 importeMasa += 10;
                 // console.log(`IMPORTE2 ${importeMasa}`)
             }else{
+                seleccion = "grande"
                 importeMasa += 15;
                 // console.log(`IMPORTE3 ${importeMasa}`)
             }
             break;
         }
     }
-
+    
     let ingrediente = document.getElementsByName("ingredient")
     for(var i=0; i<ingrediente.length; i++){
        if(ingrediente[i].checked){
@@ -131,10 +136,7 @@ function importe(){
     document.getElementById("importe_masa").textContent =`Importe de la masa: ${importeMasa} €`
     document.getElementById("importe_ingrediente").textContent =`Importe de los ingredientes:   ${importeIngredientes} €`
     document.getElementById("importe_total").textContent =`Importe TOTAL:  ${importeMasa+importeIngredientes} €`
-    //let importeTotal = `${importeMasa+importeIngredientes}`
-    //document.getElementById("importe_total").textContent =`Importe TOTAL:  ${importeTotal} €`
-
-
+    importeTotal = `${importeMasa+importeIngredientes}`;
 }
 
 
@@ -142,47 +144,47 @@ window.onload = function() {
     let validacion= false;
     formulario.onsubmit = validacion;
 
+
     submit.onclick = function(e){
         if(!validarCampo()){
             e.preventDefault();
+        }else{
+            console.log(`${importe()}`);
+            alert(`PEDIDO REALIZADO\nPizza: ${seleccion}, Importe Total: ${importeTotal}€`)
         }
     }   
     
     nombre.addEventListener("focus", 
         function(){
             document.getElementById("nombre").classList.remove("error");
-            //document.getElementById("nombre").value = ''
-            //document.getElementById("nombre").style="color: var(--color-principal); background-color: var(--color-white);";
             document.getElementById("nombre").placeholder="Nombre";
         }
     )
+
     direccion.addEventListener("focus", 
         function(){
-            document.getElementById("direccion").value = ''
-            document.getElementById("direccion").style="color: var(--color-principal); background-color: var(--color-white);";
+            document.getElementById("direccion").classList.remove("error");
             document.getElementById("direccion").placeholder="Dirección";
         }
-    )
-    tel.addEventListener("focus", 
+        )
+        
+        tel.addEventListener("focus", 
         function(){
-            document.getElementById("tel").value = ''
-            document.getElementById("tel").style="color: var(--color-principal); background-color: var(--color-white);";
+            document.getElementById("tel").classList.remove("error");
             document.getElementById("tel").placeholder="Teléfono";
         }
-    )
-    email.addEventListener("focus", 
+        )
+        
+        email.addEventListener("focus", 
         function(){
-            document.getElementById("email").value = ''
-            document.getElementById("email").style="color: var(--color-principal); background-color: var(--color-white);";
+            document.getElementById("email").classList.remove("error");
             document.getElementById("email").placeholder="E-mail";
         }
     )
-
-
-    tamanoPizza.addEventListener("click", validarRadio)
-    ingredientes.addEventListener("click", validarChecked)
     
-
+    tamanoPizza.addEventListener("click", validarRadio) 
+    ingredientes.addEventListener("click", validarChecked)
+   
     /*submit.addEventListener("click", validarRadio)
     submit.addEventListener("click", validarChecked)
     submit.addEventListener("click", importe)
@@ -211,9 +213,4 @@ window.onload = function() {
         importe()
         }
     )   
-
-
-
 };
-
-/* getElementsByTagName */
